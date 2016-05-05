827a3aa621e728ce23e60cf06db7e85dcb20cc2d
yannickdecosse <yannick.decosse@gmail.com>
2016-05-04 20:22:05 -0400

    Exercise 1: Prime number by position

---
+++ Exercises/src/Project_Euler/primeNumbers.java
@@ -0,0 +1,22 @@
+package Project_Euler;
+/*
+	Name:			Yannick Decosse
+	McGill ID:		260551160
+	Course ID:	-	
+	Instructor: 	-
+	Date:				May 4, 2016
+	Subject:		Calculating a prime number by its position, starting with 2
+ */
+
+
+/*	Import Libraries */
+
+
+public class primeNumbers {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
